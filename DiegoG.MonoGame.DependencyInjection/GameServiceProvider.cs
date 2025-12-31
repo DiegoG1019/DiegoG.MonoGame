83744e785b8e1a002f687666d36ccb2c2f303f82
Diego García <dagarciam1014@gmail.com>
2025-12-31 11:37:54 -0500

    Added unversioned files

---
+++ DiegoG.MonoGame.DependencyInjection/GameServiceProvider.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Xna.Framework;
+
+namespace DiegoG.MonoGame.DependencyInjection;
+
+public class GameServiceProvider : IServiceProvider, IKeyedServiceProvider, IDisposable, IAsyncDisposable
+{
+    public GameServiceProvider(Game game, Action<ServiceCollection>? configureServices)
+    {
+        var collection = new ServiceCollection();
+        collection.AddSingleton(game);
+        configureServices?.Invoke(collection);
+        Services = collection.BuildServiceProvider();
+        Game = game;
+    }
+    
+    public Game Game { get; }
+    public ServiceProvider Services { get; }
+
+    public object? GetService(Type serviceType)
+        => Services.GetService(serviceType) ?? Game.Services.GetService(serviceType);
+
+    public object? GetKeyedService(Type serviceType, object? serviceKey)
+        => Services.GetKeyedService(serviceType, serviceKey);
+
+    public object GetRequiredKeyedService(Type serviceType, object? serviceKey)
+        => Services.GetRequiredKeyedService(serviceType, serviceKey);
+
+    public void Dispose()
+    {
+        Services.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await Services.DisposeAsync();
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ DiegoG.MonoGame.UI/Delegates.cs
@@ -0,0 +1,8 @@
+﻿/*using Microsoft.Xna.Framework.Input;
+
+namespace DiegoG.MonoGame.UI;
+
+public delegate void StandaloneUiObjectEvent(IUiObject sender);
+public delegate void UiObjectEvent(UiManager manager, IUiObject sender);
+public delegate void UiManagerObjectEvent(UiManager manager, IUiObject? subject);
+*/
